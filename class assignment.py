88ba487f76b10ef268a5ed822a337286af957d80
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-02-10 00:18:53 -0500

    class

---
+++ class assignment.py
@@ -0,0 +1,17 @@
+
+
+
+class Animals:
+    name = ""
+    age = 3
+    color = "brown"
+
+
+class Dogs(Animals):
+    breed = 'Border Collie'
+    size = '40cm'
+
+class bird(Animals):
+    Type = 'Canary'
+    gender = 'Female'
+    
